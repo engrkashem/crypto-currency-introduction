0db2cff775980f0deab57165f7a5cf9da0a0b116
engrkashem <kashemaust@gmail.com>
2022-04-02 12:03:31 +0600

    navbar & custom link done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Coins from './components/Coins/Coins';
+import Contact from './components/Contact/Contact';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="">
+    <div className=" bg-slate-200 w-screen h-screen ">
       <Header />
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/home' element={<Home />}></Route>
+        <Route path='/coins' element={<Coins />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/about' element={<About />}></Route>
+      </Routes>
 
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import CustomLink from '../CustomLink/CustomLink';
 
 const Header = () => {
     return (
-        <nav>
-            <h2>Header nav</h2>
+        <nav className='grid grid-cols-1 md:grid-cols-2 items-center px-10 py-4 bg-white'>
+            <div className='text-4xl font-semibold text-cyan-900'>
+                <h3>Crypto Bistro</h3>
+            </div>
+            <div className='font-semibold text-xl flex gap-5 justify-end'>
+                <CustomLink to={'/home'}>Home</CustomLink>
+                <CustomLink to={'/coins'}>Coins</CustomLink>
+                <CustomLink to={'/contact'}>Contact</CustomLink>
+                <CustomLink to={'/about'}>About</CustomLink>
+            </div>
         </nav>
     );
 };
