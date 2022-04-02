2400db887e01eca893989af5bb40c2af5ba86daf
engrkashem <kashemaust@gmail.com>
2022-04-02 13:02:51 +0600

    home page done with hero Icon in the button that navigate to coins page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ArrowSmRightIcon } from '@heroicons/react/solid'
 
 const Home = () => {
+
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <h2>Home Page</h2>
+        <div className='h-[80vh] flex flex-col items-center justify-center'>
+            <h1 className='text-teal-900 text-5xl'>Welcome to <span className='text-6xl text-cyan-900'>CRYPTO BISTRO</span></h1>
+            <div className=''>
+                <button onClick={() => navigate('/coins')} className='mt-10 text-lg font-medium text-white bg-cyan-600 px-6 py-2 rounded-lg flex items-center'>Explore Coins
+                    <ArrowSmRightIcon className='w-6 h-6 ml-1' />
+                </button>
+            </div>
         </div>
     );
 };
