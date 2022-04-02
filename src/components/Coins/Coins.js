a221f26ff73218a1a17d918a26a7b1af3b4dd2d8
engrkashem <kashemaust@gmail.com>
2022-04-02 17:32:32 +0600

    done without spinner

--- src/components/Coins/Coins.js
+++ src/components/Coins/Coins.js
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useCoins from '../../hooks/useCoins';
 import CoinCard from '../CoinCard/CoinCard';
+import Spinner from '../Spinner/Spinner';
 
 const Coins = () => {
+    const [loading, setLoading] = useState(false)
 
+    // setLoading(true)
     const [coins] = useCoins();
+    // setLoading(false)
     // console.log(coins)
 
     return (
-        <div className='text-center py-10'>
-            <h2 className='text-5xl font-medium'>Crypto Currencies</h2>
-            <h6 className='text-lg mt-1 text-gray-500 font-medium'>Max Coins: {coins.length}</h6>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-20 px-10'>
-                {
-                    coins.map(coin => <CoinCard
-                        key={coin.id}
-                        coin={coin}
-                    ></CoinCard>)
-                }
-            </div>
-        </div>
+        <>
+            {
+                loading ? <Spinner /> :
+                    <div className='text-center py-10'>
+                        <h2 className='text-5xl font-medium'>Crypto Currencies</h2>
+                        <h6 className='text-lg mt-1 text-gray-500 font-medium'>Max Coins: {coins.length}</h6>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 mt-20 px-10'>
+                            {
+                                coins.map(coin => <CoinCard
+                                    key={coin.id}
+                                    coin={coin}
+                                ></CoinCard>)
+                            }
+                        </div>
+                    </div>
+            }
+        </>
     );
 };
 
--- src/hooks/useCoins.js
+++ src/hooks/useCoins.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react"
 
 const useCoins = () => {
     const [coins, setCoins] = useState([]);
-
     useEffect(() => {
         fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false')
             .then(res => res.json())
